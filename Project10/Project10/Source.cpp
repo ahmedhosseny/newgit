2e7d88761a4727ef804a2438396fab9bc3a064e0
ahmedhosseny <109736696+ahmedhosseny@users.noreply.github.com>
2022-08-04 13:55:14 +0200

    Add files via upload

---
+++ Project10/Project10/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x, z, y;
+	cout << "input 2 numbers" << endl;
+	cin >> x;
+	cin >> y;
+	z = x + y;
+	cout << "sum=" << z << endl;
+	cout << "this was the sum" << endl;
+	cout << "hellow world" << endl;
+	cout << "input 2 numbers" << endl;
+	cout << "input 2 numbers" << endl;
+}
